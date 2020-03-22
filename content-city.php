4227c220db808e792bda17cfdc122c574f8d428b
Yudin Nikita <yudin.nc@gmail.com>
2020-03-22 15:25:04 +0500

    pedicab tour

---
+++ content-city.php
@@ -0,0 +1,132 @@
+
+    <!-- Material Pricing Table End -->
+    <!-- Material Pricing Table -->
+    <section class="Material-pricing-table-section section-padding">
+      <div class="container">
+        <h1 class="section-title text-center">
+          THE CITY
+        </h1>
+        <div class="row gg-pricing-table icon-table">
+          <!-- Single Table -->
+          <div class="col-md-4 wow fadeInUp animated" data-wow-delay=".3s">
+            <div class="single-pricing-table text-center clearfix">
+                <!-- Heading -->
+                <div class="pricing-table-heading">
+                    <div class="pricing-icon">
+                        <img src="assets/images/pedicab2.jpg" alt="" class="center-block img-fluid">
+                    </div>
+                    <h2> 1.5 hour 'City Lights'</h2>
+                </div>
+                <!-- Price -->
+                <div class="price">
+                    <div class="price-item old">
+                      <div class="price-item-container">$105 <span class="price-item-type">Adult</span></div> 
+                      <div class="price-item-container">$55 <span class="price-item-type">Child</span></div>
+                    </div>
+                    <div class="price-item"></div>
+                    <div class="price-item special">
+                      <div class="price-item-head"><strong>March Special</strong></div>
+                      <div class="price-item-container">$85 <span class="price-item-type">Adult</span></div> 
+                      <div class="price-item-container">$75 <span class="price-item-type">Child under 12</span></div>
+                    </div>
+                </div>
+                <!-- Price Item -->
+                <div class="pricing-item">
+                    <ul>
+                        <li><p><strong>Private</strong> Chauffeur</p></li>
+                        <li><p><strong>Tour</strong> guide & photographer</p></li>
+                        <li><p><strong>Multiple</strong> Stops</p></li>
+                    </ul>
+                </div>
+                <!-- Button -->
+                <div class="pricing-button">
+                    <a href="javascript:void(0)" class="btn btn-common"><i class="fa fa-check"></i> Order Now<div class="ripple-container"></div></a>
+                    <a href="javascript:void(0)" class="btn"><i class="fa fa-check"></i> View Details<div class="ripple-container"></div></a>
+                </div>
+            </div>
+          </div>
+          <!-- Single Table -->
+          <div class="col-md-4 wow fadeInUp animated" data-wow-delay=".3s">
+            <div class="single-pricing-table text-center clearfix">
+                <!-- Heading -->
+                <div class="pricing-table-heading">
+                    <div class="pricing-icon">
+                        <img src="assets/images/pedicab2.jpg" alt="" class="center-block img-fluid">
+                    </div>
+                    <h2>2.5 hours 'City  Luxe'</h2>
+                </div>
+                <!-- Price -->
+                <div class="price">
+                    <div class="price-item old">
+                      <div class="price-item-container">$165 <span class="price-item-type">Adult</span></div> 
+                      <div class="price-item-container">$115 <span class="price-item-type">Child</span></div>
+                    </div>
+                    <div class="price-item"></div>
+                    <div class="price-item special">
+                      <div class="price-item-head"><strong>March Special</strong></div>
+                      <div class="price-item-container">$145 <span class="price-item-type">Adult</span></div> 
+                      <div class="price-item-container">$105 <span class="price-item-type">Child under 12</span></div>
+                    </div>
+                </div>
+                <!-- Price Item -->
+                <div class="pricing-item">
+                    <ul>
+                        <li><p><strong>Private</strong> Chauffeur</p></li>
+                        <li><p><strong>Tour</strong> guide & photographer</p></li>
+                        <li><p><strong>Unlimited</strong> Stops</p></li>
+                        <li><p><strong>Refreshments</strong></p></li>
+                        <li><p><strong>Best NY</strong> pizza</p></li>
+                    </ul>
+                </div>
+                <!-- Button -->
+                <div class="pricing-button">
+                    <a href="javascript:void(0)" class="btn btn-common"><i class="fa fa-check"></i> Order Now<div class="ripple-container"></div></a>
+                    <a href="javascript:void(0)" class="btn"><i class="fa fa-check"></i> View Details<div class="ripple-container"></div></a>
+                </div>
+            </div>
+          </div>
+          <!-- Single Table -->
+          <div class="col-md-4 wow fadeInUp animated" data-wow-delay=".3s">
+            <div class="single-pricing-table text-center clearfix">
+                <!-- Heading -->
+                <div class="pricing-table-heading">
+                    <div class="pricing-icon">
+                        <img src="assets/images/pedicab2.jpg" alt="" class="center-block img-fluid">
+                    </div>
+                    <h2>4 hours 'City 5 Star'</h2>
+                </div>
+                <!-- Price -->
+                <div class="price">
+                    <div class="price-item old">
+                      <div class="price-item-container">$265 <span class="price-item-type">Adult</span></div> 
+                      <div class="price-item-container">$195 <span class="price-item-type">Child</span></div>
+                    </div>
+                    <div class="price-item"></div>
+                    <div class="price-item special">
+                      <div class="price-item-head"><strong>March Special</strong></div>
+                      <div class="price-item-container">$240 <span class="price-item-type">Adult</span></div> 
+                      <div class="price-item-container">$175 <span class="price-item-type">Child under 12</span></div>
+                    </div>
+                </div>
+                <!-- Price Item -->
+                <div class="pricing-item">
+                    <ul>
+                        <li><p><strong>Private</strong> Chauffeur</p></li>
+                        <li><p><strong>Tour</strong> guide & photographer</p></li>
+                        <li><p><strong>Unlimited</strong> Stops</p></li>
+                        <li><p><strong>Refreshments</strong></p></li>
+                        <li><p><strong>Best NY</strong> pizza & hot dog</p></li>
+                        <li><p><strong>Complimentary</strong> Drinks*</p></li>
+                    </ul>
+                </div>
+                <!-- Button -->
+                <div class="pricing-button">
+                    <a href="javascript:void(0)" class="btn btn-common"><i class="fa fa-check"></i> Order Now<div class="ripple-container"></div></a>
+                    <a href="javascript:void(0)" class="btn"><i class="fa fa-check"></i> View Details<div class="ripple-container"></div></a>
+                </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- Material Pricing Table End -->
