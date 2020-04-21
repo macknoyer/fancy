22ebf120a40cd4075702b5cae7e0e03c89f52f6d
Yudin Nikita <yudin.nc@gmail.com>
2020-04-21 21:40:54 +0500

    technical pages

---
+++ cancel_pol.php
@@ -0,0 +1,38 @@
+<?php include('templates/header.php');?>
+      <!-- Page Titile Area -->
+      <section class="page-title-section ection-padding">
+          <div class="page-title-slider-content">
+            <div class="container">
+              <div class="row">
+                  <div class="col-md-12">
+                      <div class="title-center short">
+                          <div class="title-middle">
+                            <h1 class="page-title"> Cancellation Policy</h1>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+            </div>
+        </div>
+      </section>
+      <!-- Page Titile Area End -->
+    <section class="section-padding">
+      <div class="container">
+          <div class="row">
+              <!-- Section Titile -->
+              <div class="col-12">
+                <p><strong>Contact us by email to: <a href="mailto:info@fancyapple.com">info@fancyapple.com</a> to inquire about rescheduling/cancellation. No-shows will be charged the full price.</strong></p>
+                <p><strong>Customers will receive a full refund with at least 24 hours notice of cancellation.</strong></p>
+                <p><strong>Customers will be able to reschedule or receive a store credit with at least 4 hours notice or receive 50% refund.</strong></p>
+                <p><strong>Customers will receive a full refund in case of operator cancellation due to weather or other unforeseen circumstances.</strong></p>
+                <h4>Payment</h4>
+                <p>All tours must be pre-paid except otherwise stated. We accept Visa, Mastercard, American Express, Discover cards. Payment will be listed as "Fancy Apple" on the credit card statement.</p>
+                <h4>Vouchers</h4>
+                <p>After payment, Fancy Apple will send a confirmation / voucher by e-mail. This voucher may be printed or shown on mobile device, as a proof of purchase.</p>
+                <h4>INDEMNIFICATION</h4>
+                <p>To the fullest extent permitted by applicable law, you agree to indemnify, defend and hold harmless Fancy Apple Inc., and our respective past, present and future employees, officers, directors, contractors, consultants, equity holders, suppliers, vendors, service providers, parent companies, subsidiaries, affiliates, agents, representatives, predecessors, successors and assigns (individually and collectively, the “Fancy Apple Inc. Parties”), from and against all actual or alleged Fancy Apple Inc. Party or third party claims, damages, awards, judgments, losses, liabilities, obligations, penalties, interest, fees, expenses and costs of every kind and nature whatsoever, whether known or unknown, foreseen or unforeseen, matured or unmatured, or suspected or unsuspected, in law or equity, whether in tort, contract or otherwise (collectively, “Claims”), including, but not limited to, damages to property or personal injury, that are caused by, arise out of or are related to (a) any use or misuse of the Sites, Content, Products, or Services by you or any third party you authorize to access or use such Sites, Content or Products, (b) any User Content you create, post, share or store on or through the Sites or our pages or feeds on third party social media platforms, (c) any Feedback you provide, (d) your violation of these terms and conditions, and (e) your violation of the rights of another. You agree to promptly notify Fancy Apple Inc. of any third party Claims, cooperate with the Fancy Apple Inc. Parties in defending such Claims and pay all fees, costs and expenses associated with defending such Claims (including, but not limited to, attorneys’ fees and expenses, court costs, costs of settlement and costs of pursuing indemnification and insurance). You further agree that the Fancy Apple Inc. Parties shall have control of the defense or settlement of any third party Claims. This indemnity is in addition to, and not in lieu of, any other indemnities set forth in a written agreement between you and Fancy Apple Inc.</p>
+              </div>
+          </div>
+      </div>
+    </section>
+<?php include('templates/footer.php');?>
---
+++ terms_cond.php
@@ -0,0 +1,43 @@
+<?php include('templates/header.php');?>
+      <!-- Page Titile Area -->
+      <section class="page-title-section ection-padding">
+          <div class="page-title-slider-content">
+            <div class="container">
+              <div class="row">
+                  <div class="col-md-12">
+                      <div class="title-center short">
+                          <div class="title-middle">
+                            <h2 class="page-tagline">Last updated: 04.14.2020</h2>
+                            <h1 class="page-title">Terms &amp; Conditions</h1>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+            </div>
+        </div>
+      </section>
+      <!-- Page Titile Area End -->
+    <section class="section-padding">
+      <div class="container">
+          <div class="row">
+              <!-- Section Titile -->
+              <div class="col-12">
+                <p>Welcome to Fancy Apple Inc. and our Terms and Conditions. These terms and
+conditions are important and affect your legal rights, so please read them carefully.
+Note that our terms and conditions contain a mandatory arbitration provision that
+requires the use of arbitration on an individual basis and limits the remedies available
+to you in the event of certain disputes.</p>
+                <p>By (a) accessing or using the websites, mobile applications or blogs (collectively, the “Sites”) provided by or on behalf of Fancy Apple or our subsidiaries or other affiliates (collectively, “Fancy Apple,” “we,” “us” or “our”) that link to these terms and conditions or (b) ordering, receiving or using any sightseeing tour or tour, bike rental or bike tour (hereinafter referred to as “Services”) offered by Fancy Apple through the Sites, street ticket sellers or through any third party retailer, online or e-commerce platform, mobile application, blog or other third party channel (collectively, "Third Party Channels") ((a) and (b) together, the "Products"), you agree to be bound by these Terms and all of the terms incorporated herein by reference, including our privacy policy. If you do not agree to these terms and conditions, you may not access or use the Sites, order, receive or use the Products.
+</p>
+                <p>These terms and conditions do not alter in any way the terms or conditions of any other agreement you may have with us in respect of any products, Services or otherwise. If you are using the Sites on behalf of any person or entity, you represent and warrant that you are authorized to accept these Terms on such person or entity’s behalf and that such person or entity agrees to be responsible to us if you or such person or entity violates these terms and conditions.
+</p>
+                <p>Fancy Apple reserves the right to change or modify these terms and conditions at any time and in our sole discretion. If we make changes to these terms and conditions, we will provide notice through the Sites or updating the “Revision Date” date at the beginning of these Terms. By continuing to access or use the Sites or order, receive or use Products, you confirm your acceptance of the revised terms and conditions and all of the terms incorporated therein by reference. We encourage you to review the terms and conditions frequently to ensure that you understand the terms and conditions that apply when you access or use the Sites or order, receive or use the Products. If you do not agree to the revised terms and conditions, you may not access or use the Sites or order, receive or use the Products.
+</p>
+                <p></p>
+                <p></p>
+                <p></p>
+              </div>
+          </div>
+      </div>
+    </section>
+<?php include('templates/footer.php');?>
