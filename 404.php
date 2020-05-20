52998ac85350b6b05aa408e7352346d8b6e9c36e
Yudin Nikita <yudin.nc@gmail.com>
2020-05-20 19:41:12 +0500

    404

---
+++ 404.php
@@ -0,0 +1,32 @@
+<?php include('templates/header.php');?>
+      <!-- Page Titile Area -->
+      <section class="page-title-section ection-padding">
+          <div class="page-title-slider-content">
+            <div class="container">
+              <div class="row">
+                  <div class="col-md-12">
+                      <div class="title-center short">
+                          <div class="title-middle">
+                            <h1 class="page-title">404</h1>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+            </div>
+        </div>
+      </section>
+      <!-- Page Titile Area End -->
+
+      <!-- error section -->
+      <section class="Material-error-section section-padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <h2 class="wow animated fadeInRight" data-wow-delay=".4s">Page not found! :(</h2>
+                    <a href="javascript:void(0)" class="wow animated fadeInUp btn btn-common mt-5" data-wow-delay=".6s"><i class="material-icons mdi mdi-home"></i> Back to home<div class="ripple-container"></div></a>
+                </div>
+            </div>
+        </div>
+      </section>
+      <!-- error section end -->
+<?php include('templates/footer.php');?>
